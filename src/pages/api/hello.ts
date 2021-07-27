61403ead6aa4aac929448aaec256fb222b4aa91d
Drew Klayman <drewklay@gmail.com>
2021-07-27 16:05:58 -0400

    first commit

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import argon2 from 'argon2'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  name: string
+  password: string
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+
+  const hashedPassword = await argon2.hash('secret password');
+  res.status(200).json({ password: hashedPassword })
 }
