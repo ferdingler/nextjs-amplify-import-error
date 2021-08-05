ed85aa3ff85c40846d3b3581b98ac89a24ab68d7
Drew Klayman <drewklay@gmail.com>
2021-08-05 12:38:32 -0400

    testing fix

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ module.exports = {
 
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Important: return the modified config
-    config.externals = [...config.externals, nodeExternals()];
+    config.externals = [nodeExternals()];
 
     return config
   },
