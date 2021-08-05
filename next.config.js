88eb44af58ed937e9163487b1d5b2a4d36f10d84
Drew Klayman <drewklay@gmail.com>
2021-08-05 12:34:10 -0400

    trying to fix

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   reactStrictMode: true,
+  webpack5: false,
 
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Important: return the modified config
