665ae4666b00eabbcd25a3d205815d454956ae98
Drew Klayman <drewklay@gmail.com>
2021-08-05 13:18:22 -0400

    put in is server

--- next.config.js
+++ next.config.js
@@ -2,8 +2,10 @@ module.exports = {
   reactStrictMode: true,
 
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    if (!isServer) {
     config.externals = config.externals || [];
     config.externals = [...config.externals, 'argon2']
+    }
     return config
   },
 }
