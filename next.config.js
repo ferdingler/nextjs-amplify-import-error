d91993eb6a115a6ab3c868710f4cd42dfde164f8
Drew Klayman <drewklay@gmail.com>
2021-08-05 13:45:05 -0400

    trying

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 module.exports = {
   reactStrictMode: true,
-
+  webpack: config => {
+    config.module.rules.push({
+      test: /\.bin$/i,
+      type: 'asset/resource',
+      generator: {
+        // important, otherwise it will output into a folder that is not served by next
+        filename: 'static/[hash][ext][query]'
+      }
+    })
+    return config
+  },
 }
