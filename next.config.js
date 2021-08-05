31d228dfeae3e49616c5cc97b7e4c7da602802ac
Drew Klayman <drewklay@gmail.com>
2021-08-05 13:37:41 -0400

    removed webpack config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 module.exports = {
   reactStrictMode: true,
 
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.externals = config.externals || [];
-    config.externals = [...config.externals, 'argon2']
-    return config
-  },
 }
