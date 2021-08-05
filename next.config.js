37a988323613ff5ff2b4c3d847f3690d71de9925
Drew Klayman <drewklay@gmail.com>
2021-08-05 12:48:07 -0400

    adding prev

--- next.config.js
+++ next.config.js
@@ -6,9 +6,11 @@ module.exports = {
 
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Important: return the modified config
+    config.externals = config.externals || [];
+
     config.target = 'node';
     config.externalsPresets = { node: true };
-    config.externals = [nodeExternals()];
+    config.externals = [...config.externals, nodeExternals()];
 
     return config
   },
