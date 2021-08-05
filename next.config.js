9e8ab765202a3e3bc296cb495bb36f14927d9ad9
Drew Klayman <drewklay@gmail.com>
2021-08-05 13:03:02 -0400

    removed node externals

--- next.config.js
+++ next.config.js
@@ -1,18 +1,9 @@
-const nodeExternals = require('webpack-node-externals');
-
-
 module.exports = {
   reactStrictMode: true,
 
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Important: return the modified config
     config.externals = config.externals || [];
-
-    // config.target = 'node';
-    // config.externalsPresets = { node: true };
-    // console.
     config.externals = [...config.externals, 'argon2']
-
     return config
   },
 }
