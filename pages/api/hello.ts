48a5d034016ebaedc4b6534e0bcba3b87dd8406b
Drew Klayman <drewklay@gmail.com>
2021-08-09 21:12:15 -0400

    fixing

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -13,6 +13,6 @@ export default async function handler(
 
   const testPass = await argon2.verify({ pass: 'XX-Hk33ubmS5mTdJKZK2XqfI8', encoded: '$argon2i$v=19$m=4096,t=3,p=1$M8JE4Szs7W4dwPjkOlv5zw$I3aUBzB4FSSCEqQiNhqZ8k7J61ta0TAYLBDM+QCeBrQ' })
   .then(() => 'OK')
-  .catch(e => `${e.message}, ${e.code}`)
-  res.status(200).json({ password: testPass })
+  .catch((e) => `${e.message}, ${e.code}`)
+  return  res.status(200).send(testPass)
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import { GetServerSideProps } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
